cba52cd5a2102137d76e0e8ca9f6c6a3bed721b4
Dalmas Chituyi <122692086+dallo7@users.noreply.github.com>
2024-01-30 17:21:29 +0300

    Update app.py
    updates!

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pickle
 import requests
 import smtplib
 import ssl
+import time
 from email.message import EmailMessage
 
 
@@ -69,144 +70,154 @@ def send_sms(phonenumber: int, sms: str):
     return response
 
 
-img = "pvs.png"
-
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='Rhys')
 
 server = app.server
 
+
 app.layout = dbc.Container([
+    dbc.Row([
+        html.Div([html.P(['Importer Quotation Checker!'], id='import',
+                         style={'marginBottom': 10, 'marginTop': 10, 'font-family': 'cursive',
+                                'text-align': 'center', 'color': '#00FFFF',
+                                'fontSize': 20})]),
+        html.Br(),
+        html.Br(),
+        dbc.Row([
+            dbc.Col([
+                html.Div([
+                    dbc.Label('Product'),
+                    dcc.Dropdown(['metal', 'oil', 'gas', 'maize', 'wheat'
+                                  ], multi=False, placeholder='Product', id='product', clearable=True,
+                                 optionHeight=50),
+                ], style={'marginBottom': 10, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
+                dbc.FormText('Please enter product', style={'marginBottom': 10, 'marginTop': 5}),
+                html.Div([
+                    dbc.Label('Quote'),
+                    dcc.Dropdown([1000, 2000, 2500, 3000, 4500, 4000, 5000, 6000, 0
+                                  ], multi=False, placeholder='Quote', id='quote', clearable=True, optionHeight=50)
+                ], style={'marginBottom': 10, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
+                dbc.FormText('Please enter quote'),
+                html.Div(id="alert"),
+                html.Div([
+                    dbc.Label('Enter Mail',
+                              style={'color': 'Green', 'marginBottom': 10, 'marginTop': 10, 'fontSize': 14}),
+                    dcc.Input(placeholder='Mail', id='mail',
+                              style={'marginBottom': 10, 'marginTop': 5, 'width': '100%', 'height': '36px',
+                                     'color': 'Green', 'borderRadius': '4px',
+                                     'border': '1px solid rgba(0, 0, 0, 0.5)', 'paddingLeft': '10px',
+                                     'fontSize': 14})
+                ]),
+                dbc.FormText('Please enter mail'),
+                html.Div([
+                    dbc.Label('Enter Telephone No:'),
+                    dcc.Input(placeholder='Tel', id='tel',
+                              style={'marginBottom': 10, 'marginTop': 5, 'width': '100%', 'height': '36px',
+                                     'color': 'Green', 'borderRadius': '4px',
+                                     'border': '1px solid rgba(0, 0, 0, 0.5)', 'paddingLeft': '10px',
+                                     'fontSize': 14})
+                ], style={'marginBottom': 5, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
+                dbc.FormText('Please TelNo.'),
+                html.Br(),
+                html.Br(),
+                dbc.Button('Submit quote', id='submitTextarea', n_clicks=1)
+            ], style={'width': '100%', 'height': 200, 'marginBottom': 15,
+                      'marginTop': 15, 'color': '#00FFFF',
+                      'font-family': 'cursive', 'fontSize': 14}),
 
-    html.Br(),
-
-    dbc.Row(
-        [
-            html.Div([html.P(['Importer Quotation Checker!'], id='import',
-                             style={'marginBottom': 10, 'marginTop': 10, 'font-family': 'cursive',
-                                    'text-align': 'center', 'color': '#00FFFF',
-                                    'fontSize': 20})]),
-            html.Br(),
-            html.Br(),
-            dbc.Row([
-                dbc.Col([
-                    html.Div([
-                        dbc.Label('Product'),
-                        dcc.Dropdown(['metal', 'oil', 'gas', 'maize', 'wheat'
-                                      ], multi=False, placeholder='Product', id='product', clearable=True,
-                                     optionHeight=50),
-                    ], style={'marginBottom': 10, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
-                    dbc.FormText('Please enter product', style={'marginBottom': 10, 'marginTop': 5}),
-                    html.Div([
-                        dbc.Label('Quote'),
-                        dcc.Dropdown([1000, 2000, 2500, 3000, 4500, 4000, 5000, 6000, 0
-                                      ], multi=False, placeholder='Quote', id='quote', clearable=True, optionHeight=50)
-                    ], style={'marginBottom': 10, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
-                    dbc.FormText('Please enter quote'),
-                    html.Div([
-                        dbc.Label('Enter Mail',
-                                  style={'color': 'Green', 'marginBottom': 10, 'marginTop': 10, 'fontSize': 14}),
-                        dcc.Input(placeholder='Mail', id='mail',
-                                  style={'marginBottom': 10, 'marginTop': 5, 'width': '100%', 'height': '36px',
-                                         'color': 'Green', 'borderRadius': '4px',
-                                         'border': '1px solid rgba(0, 0, 0, 0.5)', 'paddingLeft': '10px',
-                                         'fontSize': 14})
-                    ]),
-                    dbc.FormText('Please enter mail'),
-                    html.Div([
-                        dbc.Label('Enter Telephone No:'),
-                        dcc.Input(placeholder='Tel', id='tel',
-                                  style={'marginBottom': 10, 'marginTop': 5, 'width': '100%', 'height': '36px',
-                                         'color': 'Green', 'borderRadius': '4px',
-                                         'border': '1px solid rgba(0, 0, 0, 0.5)', 'paddingLeft': '10px',
-                                         'fontSize': 14})
-                    ], style={'marginBottom': 5, 'marginTop': 5, 'color': 'Green', 'fontSize': 14}),
-                    dbc.FormText('Please TelNo.'),
-                    html.Br(),
-                    html.Br(),
-                    dbc.Button('Submit quote', id='submitTextarea')
-                ], style={'width': '100%', 'height': 200, 'marginBottom': 15,
-                          'marginTop': 15, 'color': '#00FFFF',
-                          'font-family': 'cursive', 'fontSize': 14}),
-
-                dbc.Col([
-                    dbc.Label('Model Response',
-                              style={'marginBottom': 15, 'color': '#800040', 'marginTop': 15, 'fontSize': 16}),
-                    html.Div(id='parsed',
-                             style={'marginBottom': 15, 'color': '#080040', 'marginTop': 15, 'fontSize': 14}),
-                    html.Br(),
-                    html.Br(),
-                    dbc.Label('Email is sent to Importer and Customs',
-                              style={'marginBottom': 15, 'color': '#800040', 'marginTop': 15, 'fontSize': 16}),
-                    html.Div(id='email',
-                             style={'marginBottom': 15, 'color': '#080040', 'marginTop': 15, 'fontSize': 14}),
-                    html.Br(),
-                ], style={'width': '100%', 'height': 200, 'marginBottom': 15,
-                          'marginTop': 15, 'color': '#00FFFF', 'text-align': 'center',
-                          'font-family': 'cursive', 'fontSize': 14})
-            ]),
-        ])
-
+            dbc.Col([
+                dbc.Label('Model Response',
+                          style={'marginBottom': 15, 'color': '#800040', 'marginTop': 15, 'fontSize': 16}),
+                html.Div(id='parsed',
+                         style={'marginBottom': 15, 'color': '#080040', 'marginTop': 15, 'fontSize': 14}),
+                html.Br(),
+                html.Br(),
+                dbc.Label('Email is sent to Importer and Customs',
+                          style={'marginBottom': 15, 'color': '#800040', 'marginTop': 15, 'fontSize': 16}),
+                html.Div(id='payload',
+                         style={'marginBottom': 15, 'color': '#080040', 'marginTop': 15, 'fontSize': 14}),
+                html.Br(),
+            ], style={'width': '100%', 'height': 200, 'marginBottom': 15,
+                      'marginTop': 15, 'color': '#00FFFF', 'text-align': 'center',
+                      'font-family': 'cursive', 'fontSize': 14})
+        ]),
+    ])
 ], style={'backgroundColor': 'F0E68C', 'marginTop': '20px', 'font-family': 'cursive', 'marginBottom': '40px',
           'border': '2px', 'color': 'cyan'})
 
 
-@app.callback(Output('parsed', 'children'),
-              Output('email', 'children'),
+@app.callback(Output('alert', 'children'),
+              Output('parsed', 'children'),
+              Output('payload', 'children'),
               Input('quote', 'value'),
               Input('product', 'value'),
               Input('mail', 'value'),
               Input('tel', 'value'),
               Input('submitTextarea', 'n_clicks'), prevent_initial_call=True)
 def update_output(quote, product, mail, tel, n_clicks):
-    if n_clicks:
+    if n_clicks == 2:
 
-        test = {'quote': quote, 'product': product}
+        if mail and tel:
 
-        new_df = pd.DataFrame(test, index=[8])
+            test = {'quote': quote, 'product': product}
 
-        amp = pd.get_dummies(new_df)
+            new_df = pd.DataFrame(test, index=[8])
 
-        amp = amp.to_dict()
+            amp = pd.get_dummies(new_df)
 
-        sch = {'quote': {8: 0},
-               'product_gas': {8: 0},
-               'product_maize': {8: 0},
-               'product_metal': {8: 0},
-               'product_oil': {8: 0},
-               'product_wheat': {8: 0}}
+            amp = amp.to_dict()
 
-        test_df = {**sch, **amp}
+            sch = {'quote': {8: 0},
+                   'product_gas': {8: 0},
+                   'product_maize': {8: 0},
+                   'product_metal': {8: 0},
+                   'product_oil': {8: 0},
+                   'product_wheat': {8: 0}}
 
-        pred = pd.DataFrame(test_df, index=[8])
+            test_df = {**sch, **amp}
 
-        with open('predicktor.pkl', 'rb') as f:
-            predictor_load = pickle.load(f)
+            pred = pd.DataFrame(test_df, index=[8])
 
-        parsed = predictor_load.predict(pred)[0]
+            with open('predicktor.pkl', 'rb') as f:
+                predictor_load = pickle.load(f)
 
-        if parsed == 1:
-            parsed = "Accepted"
-            receiverAddress = mail
-            subject = "Quotation Accepted!"
-            body = "Congratulations! Your quotation for this product is under review!"
-            send_sms(tel, body)
-            sendMail(receiverAddress, subject, body)
+            parsed = predictor_load.predict(pred)[0]
+
+            if parsed == 1:
+                parsed = "Accepted"
+                receiverAddress = mail
+                subject = "Quotation Accepted!"
+                body = "Congratulations! Your quotation for this product is under review!"
+                send_sms(tel, body)
+                sendMail(receiverAddress, subject, body)
+                reject = "your quote for this project is accepted"
+
+            else:
+                parsed = "Rejected"
+                receiverAddress = mail
+                subject = "Quotation Rejected!"
+                body = "Please review the amount quoted for this product and re-submit!"
+                send_sms(tel, body)
+                sendMail(receiverAddress, subject, body)
+                reject = "your quote for this project is rejected"
+
+            payload = f"Your quote for this product is {parsed}"
+
+            alert = html.Div([html.Br(),
+                              html.Hr(),
+                              dbc.Alert(id="toss", children=reject, color="primary"),
+                              html.Hr(),
+                              html.Br()])
+
+            time.sleep(5)
+
+            return alert, parsed, payload,
 
         else:
-            parsed = "Rejected"
-            receiverAddress = mail
-            subject = "Quotation Rejected!"
-            body = "Please review the amount quoted for this product and re-submit!"
-            send_sms(tel, body)
-            sendMail(receiverAddress, subject, body)
-
-        payload = f"Your quote for this product is {parsed}"
-
-        return parsed, payload
-
+            return dash.no_update
     else:
-        return dash.no_update, dash.no_update
+        return dash.no_update
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=5050)
+    app.run_server(debug=True, port=5050, host="192.168.11.108")
